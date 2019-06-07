18d826bdfd0ec35dfa1c0381627feac0bc02846c
Thomas Jakway <tjakway@nyu.edu>
2019-06-07 16:52:25 -0400

    debugging functions

--- src/map-to-output/MapToOutputUtil.cpp
+++ src/map-to-output/MapToOutputUtil.cpp
@@ -4,6 +4,8 @@
 
 #include <sstream>
 
+#include <gtk/gtk.h>
+
 class BadDimensionsError : public MapToOutputError
 {
 public:
@@ -28,4 +30,34 @@ void MapToOutputUtil::checkDimensions(
             throw BadDimensionsError(ss.str());
         }
     }
+}
+
+std::pair<double, double> MapToOutputUtil::getCairoProjection(cairo_t* cairo)
+{
+    double x = -1, y = -1;
+
+    cairo_user_to_device(cairo, &x, &y);
+
+    return std::make_pair(x, y);
+}
+
+//from https://mail.gnome.org/archives/gtk-app-devel-list/2004-November/msg00028.html
+GdkRectangle MapToOutputUtil::widgetGetRectInScreen(GtkWidget *widget)
+{
+    GdkRectangle r;
+    gint                      x,y,w,h;
+    GdkRectangle    extents;
+    GdkWindow       *window;
+
+    window = gtk_widget_get_parent_window(widget); /* getting parent window */
+    gdk_window_get_root_origin(window, &x,&y); /* parent's left-top screen coordinates */
+    gdk_drawable_get_size(window, &w,&h); /* parent's width and height */
+    gdk_window_get_frame_extents(window, &extents); /* parent's extents (including decorations) */
+    r.x = x + (extents.width-w)/2 + widget->allocation.x; /* calculating x (assuming: left border size == right border size) */
+    r.y = y + (extents.height-h)-(extents.width-w)/2 + widget->allocation.y; /* calculating y (assuming: left border size == right border size == bottom border size) */
+
+    r.width = widget->allocation.width;
+    r.height = widget->allocation.height;
+
+    return r;
 }
--- src/map-to-output/MapToOutputUtil.h
+++ src/map-to-output/MapToOutputUtil.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <utility>
+#include <cairo/cairo.h>
+
 class MapToOutputUtil
 {
     MapToOutputUtil() = delete;
 public:
     static void checkDimensions(bool, int, int, int, int);
+
+    static std::pair<double, double> getCairoProjection(cairo_t*);
+
+    GdkRectangle widgetGetRectInScreen(GtkWidget *widget);
+
 };
+
