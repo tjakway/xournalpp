194bdc9cb0fa5d1a9ff55a0fc9b353d659b4f0bd
Thomas Jakway <tjakway@nyu.edu>
2019-06-12 13:46:11 -0400

    writing WithXSetWacomController

--- src/map-to-output/WithXSetWacomController.cpp
+++ src/map-to-output/WithXSetWacomController.cpp
@@ -1,12 +1,24 @@
 #include "map-to-output/WithXSetWacomController.h"
 #include "map-to-output/MapToOutputConfig.h"
 
+#include <memory>
+#include <unordered_set>
+
+static const MapToOutputConfig::DeviceRegexes myDeviceRegexes {
+    std::unordered_set<std::string> {
+        "Wacom Intuos S 2 Pad pad",
+        "Wacom Intuos S 2 Pen stylus"
+    },
+    "Wacom Intuos S 2 Pen stylus"
+};
+    
+
 WithXSetWacomController::WithXSetWacomController(bool enabled)
+    //TODO: read settings
+    : config(myDeviceRegexes)
 {
     if(enabled)
     {
-        //TODO: read settings
-        
         controller = 
             std::unique_ptr<XSetWacomController>(new XSetWacomController)
     }
--- src/map-to-output/WithXSetWacomController.h
+++ src/map-to-output/WithXSetWacomController.h
@@ -3,10 +3,12 @@
 #include <memory>
 
 #include "map-to-output/XSetWacomController.h"
+#include "map-to-output/MapToOutputConfig.h"
 
 class WithXSetWacomController
 {
     std::unique_ptr<XSetWacomController> controller;
+    MapToOutputConfig::Ptr config;
 
 public:
     WithXSetWacomController(bool);
